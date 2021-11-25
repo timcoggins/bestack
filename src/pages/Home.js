9d7c6b9d5a2549653a0c66b4b04ecf4a30a7443a
Tim Coggins <timcoggins@gmail.com>
2021-11-25 12:41:32 +0100

    Basic home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
-
+import PageContainer from "../components/atoms/PageContainer";
+import H1 from "../components/atoms/H1"
+import P from "../components/atoms/P"
 
 const Home = () => {
+    return (
+        <PageContainer>
+            <H1>Welcome To the game</H1>
+            <P>Choose a game below</P>
 
+
+        </PageContainer>
+    )
 
 
 }
