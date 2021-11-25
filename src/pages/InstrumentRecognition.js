fb9d71132ac598e30974bf35bc5f60c62a6fc5ce
Pierre van Male <vmalep@protonmail.com>
2021-11-25 22:07:33 +0100

    Finishe or Next button done

--- src/pages/InstrumentRecognition.js
+++ src/pages/InstrumentRecognition.js
@@ -2,21 +2,21 @@
  * InstrumentRecognition.js
  * Game page
  */
-import { useState, useContext } from 'react'
-import InstrumentRecognitionList from "../assets/instrumentRecognitionList"
-import generateRandomInstrument from '../utils/generateRandomInstrument'
-import ScoreListContext from '../contexts/ScoreContext'
+import { useState, useContext } from "react";
+import InstrumentRecognitionList from "../assets/instrumentRecognitionList";
+import generateRandomInstrument from "../utils/generateRandomInstrument";
+import ScoreListContext from "../contexts/ScoreContext";
 
-import StyledLink from '../components/atoms/StyledLink';
-import PageContainer from "../components/atoms/PageContainer"
-import H1 from "../components/atoms/H1"
-import P from "../components/atoms/P"
-import Instrument from "../components/atoms/Instrument"
-import InstrumentContainer from "../components/atoms/InstrumentContainer"
-import InstrumentImg from "../components/atoms/InstrumentImg"
-import Button from "../components/atoms/Button"
-import Center from '../components/atoms/Center'
-import Controls from '../components/atoms/Controls'
+import StyledLink from "../components/atoms/StyledLink";
+import PageContainer from "../components/atoms/PageContainer";
+import H1 from "../components/atoms/H1";
+import P from "../components/atoms/P";
+import Instrument from "../components/atoms/Instrument";
+import InstrumentContainer from "../components/atoms/InstrumentContainer";
+import InstrumentImg from "../components/atoms/InstrumentImg";
+import Button from "../components/atoms/Button";
+import Center from "../components/atoms/Center";
+import Controls from "../components/atoms/Controls";
 
 /**
  * InstrumentRecognition
@@ -24,102 +24,111 @@ import Controls from '../components/atoms/Controls'
  * @constructor
  */
 const InstrumentRecognition = () => {
+  // Consume the context
+  const { instrumentSettings, scoreList, setScoreList } =
+    useContext(ScoreListContext);
+  const [resultBtnDisabled, setResultBtnDisabled] = useState(true);
 
-    // Consume the context
-    const { instrumentSettings, scoreList, setScoreList } = useContext(ScoreListContext);
-    const [resultBtnDisabled, setResultBtnDisabled] = useState(true);
+  console.log(instrumentSettings);
 
-    console.log(instrumentSettings);
+  const FilteredInstruments = InstrumentRecognitionList.filter((item) => {
+    if (instrumentSettings.difficulty === "easy" && item.difficulty > 1)
+      return false;
+    if (instrumentSettings.difficulty === "medium" && item.difficulty > 2)
+      return false;
+    if (instrumentSettings.difficulty === "hard" && item.difficulty > 3)
+      return false;
+    return true;
+  });
 
-    const FilteredInstruments = InstrumentRecognitionList.filter(item => {
-        if(instrumentSettings.difficulty === 'easy' && item.difficulty > 1 ) return false
-        if(instrumentSettings.difficulty === 'medium' && item.difficulty > 2 ) return false
-        if(instrumentSettings.difficulty === 'hard' && item.difficulty > 3 ) return false
-        return true;
-    })
+  // Declare state variables
+  const [question, setQuestion] = useState(
+    FilteredInstruments[generateRandomInstrument(FilteredInstruments)]
+  );
+  const [msg, setMsg] = useState("");
+  const [score, setScore] = useState(0);
+  const [questions, setQuestions] = useState(0);
 
-    // Declare state variables
-    const [ question, setQuestion ] = useState(FilteredInstruments[generateRandomInstrument(FilteredInstruments)]);
-    const [ msg, setMsg ] = useState('')
-    const [ score, setScore ] = useState(0)
-    const [ questions, setQuestions ] = useState(0)
+  /**
+   * Checks the users answer when they click on a button
+   * @param answer
+   */
+  const checkAnswer = (answer) => {
+    // If the user has already chosen an answer dont listen to another
+    if (msg !== "") return;
 
-
-    /**
-     * Checks the users answer when they click on a button
-     * @param answer
-     */
-    const checkAnswer = (answer) => {
-        // If the user has already chosen an answer dont listen to another
-        if(msg !== '') return
-
-        // Check the answer
-        if (answer === question.name) {
-            setMsg('Correct')
-            setScore(score+1);
-            setQuestions(questions + 1)
-        } else {
-            setMsg('Incorrect')
-            setQuestions(questions + 1)
-        }
+    // Check the answer
+    if (answer === question.name) {
+      setMsg("Correct");
+      setScore(score + 1);
+      setQuestions(questions + 1);
+    } else {
+      setMsg("Incorrect");
+      setQuestions(questions + 1);
     }
+  };
 
-    /**
-     * Goes to the next question or the results page
-     */
-    const nextQuestion = () => {
-        if(questions !== instrumentSettings.numberOfQuestions) {
-            setMsg('')
-            setQuestion(FilteredInstruments[generateRandomInstrument(FilteredInstruments)])
-        } else {
-            setMsg('Finished!')
-            setScoreList([...scoreList, {
-                gameId: '2',
-                difficultyLevel: instrumentSettings.difficulty,
-                questNb: questions,
-                ScoreInPc: score
-            }])
-            setResultBtnDisabled(false);
-            //setInterval(() => window.location = '/results', 2000);
-        }
+  /**
+   * Goes to the next question or the results page
+   */
+  const nextQuestion = () => {
+    if (questions !== instrumentSettings.numberOfQuestions) {
+      setMsg("");
+      setQuestion(
+        FilteredInstruments[generateRandomInstrument(FilteredInstruments)]
+      );
+    } else {
+      setMsg("Finished!");
+      const newScore = {
+        gameId: "1",
+        difficultyLevel: instrumentSettings.difficulty,
+        scoreInPc: (score / instrumentSettings.numberOfQuestions) * 100,
+      };
+      const newScoreList = scoreList.slice();
+      newScoreList.unshift(newScore);
+      setScoreList(newScoreList);
+      setResultBtnDisabled(false);
     }
+  };
 
+  // JSX
+  return (
+    <PageContainer>
+      <P>Listen closely and pick which instrument is playing</P>
+      {/*<P>Difficulty: {instrumentSettings.difficulty}</P>*/}
+      {/*<P>Score: {score}</P>*/}
+      {/*<P>Questions: {questions} / {instrumentSettings.numberOfQuestions} </P>*/}
 
-    // JSX
-    return(
-        <PageContainer>
-            <P>Listen closely and pick which instrument is playing</P>
-            {/*<P>Difficulty: {instrumentSettings.difficulty}</P>*/}
-            {/*<P>Score: {score}</P>*/}
-            {/*<P>Questions: {questions} / {instrumentSettings.numberOfQuestions} </P>*/}
+      <Center>
+        {!msg && (
+          <audio controls>
+            <source src={question.audio} type="audio/wav" />
+          </audio>
+        )}
+      </Center>
 
-            <Center>
-                {!msg && <audio controls>
-                    <source src={question.audio} type="audio/wav" />
-                </audio>}
-            </Center>
+      <InstrumentContainer>
+        {FilteredInstruments.map((item, index) => (
+          <Instrument key={index} onClick={() => checkAnswer(item.name)}>
+            <InstrumentImg src={`${item.icon}`} />
+            <P>{item.name}</P>
+          </Instrument>
+        ))}
+      </InstrumentContainer>
 
-            <InstrumentContainer>
-                {FilteredInstruments.map((item, index) => 
-                    <Instrument key={index} onClick={() => checkAnswer(item.name)}>
-                        <InstrumentImg src={`${item.icon}`} />
-                        <P>{item.name}</P>
-                    </Instrument>
-                )}
-            </InstrumentContainer>
+      <P>{msg}</P>
 
-            <P>{msg}</P>
+      <Controls middle>
+        {!resultBtnDisabled ? (
+          <StyledLink to="/results">
+            <Button>Finish</Button>
+          </StyledLink>
+        ) : (
+          <Button onClick={() => nextQuestion()}>Next</Button>
+        )}
+      </Controls>
+    </PageContainer>
+  );
+};
 
-            <Controls middle>
-                <StyledLink to='/results' ><Button disabled={resultBtnDisabled}>Finish</Button></StyledLink>
-                <Button onClick={() => nextQuestion()}>Next</Button>
-            </Controls>
-
-        </PageContainer>
-    )
-
-
-}
-
-export default InstrumentRecognition
-
+export default InstrumentRecognition;
--- src/pages/NoteRecognition.js
+++ src/pages/NoteRecognition.js
@@ -1,96 +1,107 @@
 /**
  * NoteRecognition.js
  */
-import { useState, useContext } from 'react'
-import generateRandomNote from '../utils/generateRandomNote';
+import { useState, useContext } from "react";
+import generateRandomNote from "../utils/generateRandomNote";
 import ScoreListContext from "../contexts/ScoreContext";
-import StyledLink from '../components/atoms/StyledLink';
+import StyledLink from "../components/atoms/StyledLink";
 
 import PageContainer from "../components/atoms/PageContainer";
 import P from "../components/atoms/P";
 import Button from "../components/atoms/Button";
-import SheetMusic from 'react-sheet-music';
-import Controls from '../components/atoms/Controls'
+import SheetMusic from "react-sheet-music";
+import Controls from "../components/atoms/Controls";
 
 /**
  * NoteRecognition
  * @returns {JSX.Element}
  */
 const NoteRecognition = () => {
+  // Consume context
+  const { noteSettings, scoreList, setScoreList } =
+    useContext(ScoreListContext);
 
-    // Consume context
-    const { noteSettings, scoreList, setScoreList } = useContext(ScoreListContext);
+  console.log(noteSettings);
 
-    console.log(noteSettings);
+  // State variables
+  const [note, setNote] = useState(`${generateRandomNote("easy", "piano")}`);
+  const [msg, setMsg] = useState("");
+  const [score, setScore] = useState(0);
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
+  const [resultBtnDisabled, setResultBtnDisabled] = useState(true);
+  const [questCount, setQuestCount] = useState(1);
 
-    // State variables
-    const [ note, setNote ] = useState(`${generateRandomNote('easy', 'piano')}`);
-    const [ msg, setMsg ] = useState('');
-    const [ score, setScore ] = useState(0);
-    const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
-    const [resultBtnDisabled, setResultBtnDisabled] = useState(true);
-    const [ questCount, setQuestCount ] = useState(1);
-
-    /**
-     * Check user choice
-     * @param input
-     */
-    const checkNote = (input) => {
-        if(input === note) {
-            setMsg('Correct!')
-            setScore(score + 1)
-        } else {
-            setMsg('Incorrect')
-        }
-        setNextBtnDisabled(false);
-        console.log('questCount / NbOfQuest: ' + questCount + ' / ' + noteSettings.numberOfQuestions);
-        if(questCount >= noteSettings.numberOfQuestions) {
-            console.log("finished");
-            const newScore = ({gameId: '1', scoreInPc: score / noteSettings.numberOfQuestions * 100});
-            const newScoreList = scoreList.slice();
-            newScoreList.push(newScore);
-            setScoreList(newScoreList);
-            setResultBtnDisabled(false);
-        }
+  /**
+   * Check user choice
+   * @param input
+   */
+  const checkNote = (input) => {
+    if (input === note) {
+      setMsg("Correct!");
+      setScore(score + 1);
+    } else {
+      setMsg("Incorrect");
     }
+    setNextBtnDisabled(false);
+    console.log(
+      "questCount / NbOfQuest: " +
+        questCount +
+        " / " +
+        noteSettings.numberOfQuestions
+    );
+    if (questCount >= noteSettings.numberOfQuestions) {
+      console.log("finished");
+      const newScore = {
+        gameId: "1",
+        difficultyLevel: noteSettings.difficulty,
+        scoreInPc: (score / noteSettings.numberOfQuestions) * 100,
+      };
+      const newScoreList = scoreList.slice();
+      newScoreList.unshift(newScore);
+      setScoreList(newScoreList);
+      setResultBtnDisabled(false);
+    }
+  };
 
-    // JSX
-    return (
-        <PageContainer>
-            <P>Go ahead, try to identify the notes!</P>
-            <P>Score: {score}</P>
+  // JSX
+  return (
+    <PageContainer>
+      <P>Go ahead, try to identify the notes!</P>
+      <P>Score: {score}</P>
 
-                <SheetMusic
-                notation={`${note}4`}
-                />
+      <SheetMusic notation={`${note}4`} />
 
-            <Controls grid style={{marginTop: '0'}}>
-                <Button onClick={() => checkNote('C')}>C</Button>
-                <Button onClick={() => checkNote('D')}>D</Button>
-                <Button onClick={() => checkNote('E')}>E</Button>
-                <Button onClick={() => checkNote('F')}>F</Button>
-                <Button onClick={() => checkNote('G')}>G</Button>
-                <Button onClick={() => checkNote('A')}>A</Button>
-                <Button onClick={() => checkNote('B')}>B</Button>
-            </Controls>
+      <Controls grid style={{ marginTop: "0" }}>
+        <Button onClick={() => checkNote("C")}>C</Button>
+        <Button onClick={() => checkNote("D")}>D</Button>
+        <Button onClick={() => checkNote("E")}>E</Button>
+        <Button onClick={() => checkNote("F")}>F</Button>
+        <Button onClick={() => checkNote("G")}>G</Button>
+        <Button onClick={() => checkNote("A")}>A</Button>
+        <Button onClick={() => checkNote("B")}>B</Button>
+      </Controls>
 
+      {msg ? <p>{msg}</p> : ""}
+      <Controls middle>
+        {!resultBtnDisabled ? (
+          <StyledLink to="/results">
+            <Button>Results</Button>
+          </StyledLink>
+        ) : (
+          <Button
+            disabled={nextBtnDisabled}
+            onClick={() => {
+              setNote(`${generateRandomNote("easy", "piano")}`);
+              setMsg("");
+              setQuestCount(questCount + 1);
+            }}
+          >
+            Next Note
+          </Button>
+        )}
+      </Controls>
+    </PageContainer>
+  );
+};
 
-            {msg ? <p>{msg}</p> : ''}
-            <Controls middle>
-            <StyledLink to='/results' ><Button disabled={resultBtnDisabled}>Results</Button></StyledLink>
-            <Button
-                disabled={nextBtnDisabled}
-                onClick={() => {
-                    setNote(`${generateRandomNote('easy', 'piano')}`);
-                    setMsg('');
-                    setQuestCount(questCount + 1);
-                }}
-            >
-                Next Note
-            </Button>
-            </Controls>
-        </PageContainer>
-    )
-}
-
-export default NoteRecognition
+export default NoteRecognition;
