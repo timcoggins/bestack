9fd8c2cc55eacdd9a72bd0aab11c38960dcfe028
Pierre van Male <vmalep@protonmail.com>
2021-11-25 22:39:19 +0100

    simpler generateRandomNote

--- src/utils/generateRandomNote.js
+++ src/utils/generateRandomNote.js
@@ -1,13 +1,6 @@
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
-
 const generateRandomNote = (difficulty, instrument) => {
-    getRandomInt(0, 6)
     const notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
-    return notes[getRandomInt(0, 6)]
+    return notes[Math.floor(Math.random() * notes.length)]
 }
 
 export default generateRandomNote
